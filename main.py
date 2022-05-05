17ad8f6491226a8954a680cb43a64ce5b3608b7b
HackCreep <hackcreep14@gmail.com>
2022-05-05 10:05:52 +0800

    Added music download :0

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-import os
+import json
 import discord
+import os
 from discord.ext import commands
 
-
-# something
-class helppo(commands.HelpCommand):
-    def __init__(self):
-        super().__init__()
-
-    async def send_bot_help(self, mapping):
-        for cog in mapping:
-            await self.get_destination().send(f'')
-
-    async def send_cog_help(self, cog):
-        return await super().send_cog_help(cog)
-
-    async def send_group_help(self, group):
-        return await super(helppo, self).send_group_help(group)
-
-    async def send_command_help(self, command):
-        return await super().send_command_help(command)
-
-
 prefix = "h!"
-client = commands.Bot(command_prefix=prefix, description="Hello", case_insensitive=False, help_command=helppo())
+client = commands.Bot(command_prefix=prefix, description="Hello", case_insensitive=False)
+client.remove_command("help")
 
 
 @client.event
@@ -32,13 +14,22 @@ async def on_ready():
         activity=discord.Streaming(name="Death", url="https://www.youtube.com/watch?v=1G-tJsFfo2I"))
     print('I logged on as a hunter')  # idk
 
+
+# Commands
+
 @client.command()
-async def Explode(ctx):
+async def explode(ctx, * message):
     await ctx.send("don't make him do that 3:|")
+    await message.delete()
+
+
+# TODO Organfailureisnotwhatimgonnabehaving
+
+# cog initiator
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-# e | Run
-client.run('[Insert token here]')
+# run bot
+client.run('insert token')
